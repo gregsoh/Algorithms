f0ca3c72d2b5b19a732029199c7641b810c8040f
gregsoh <46077253+gregsoh@users.noreply.github.com>
2019-05-01 13:57:08 -0700

    Added new code

--- BLR.py
+++ BLR.py
@@ -1,8 +1,21 @@
 '''
 This code implements beyesian linear regression (just a simple toy example). 
+THIS CODE IS IN PROGRESS!!!!!!!!!! (I.E. IT IS STILL NOT FUNCTIONAL)
 '''
 import numpy as np
 np.random.seed(1)
+
+class LB(object):
+    def __init__(self, prior_mean, prior_covariance, noise):
+        self.prior = mv_norm(mean = a_m0, cov=m_S0)
+        self.v_m0 = a_m0.reshape(a_m0.shape + (1,)) #reshape to column vector
+        self.m_S0 = m_S0
+        self.beta = beta
+        
+        self.v_mN = self.v_m0
+        self.m_SN = self.m_S0
+        self.posterior = self.prior
+
 
 # Fn 1: Generating data (this is not predicted by model)
 #       Assume y = b_0 + b_1x + e (where e is normal distributed with mean 0, variance 0.1)
